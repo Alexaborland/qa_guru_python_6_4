6f015abcfe2972a794048a8879959b16aebc266d
Alexaborland <Borland3711@gmail.com>
2023-06-28 16:42:22 +0300

    test homework

---
+++ fgbhfgb.py
@@ -0,0 +1,8 @@
+import random
+def test_unique_elements():
+    """
+    Удалите из списка все повторяющиеся элементы
+    """
+    l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
+    l = list(set(l))
+    print(l)
